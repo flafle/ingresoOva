5cffd9d84545761f456e147150296bce9efcdb2b
FlaFe <104147078+flafle@users.noreply.github.com>
2024-02-28 12:05:40 -0300

    Ingreso Ova

---
+++ js/sweetAlert.js
@@ -0,0 +1,78 @@
+
+// (async() => {
+//    const {value:dni} = await Swal.fire({
+//       title:"Bienvenid@ !",
+//       //text:"Ingrese su número de documento",
+//      //  html:`<input id="swal-input1" class="swal2-input" placeholder="Ingrese su número de documento">`,
+//        // icon:
+//        confirmButtonText:"Seleccionar",
+//        footer:'<span class="informacion">Escriba su numero sin puntos ni espacios',
+//        width:'70%',
+//         padding: "2rem",
+//        // background:
+//         // grow:"column",
+//         backdrop:true,
+//        // timer:
+//        // timerProgressBar:
+//        // toast:
+//        // position:
+
+
+//        allowOutsideClick:false,
+//        allowEscapeKey:false,
+//        allowEnterKey:false,
+//        stopKeydownPropagation:false,
+   
+//        input: 'text',
+//        inputPlaceholder:"Ingrese su número de documento",
+//         inputValue:"",
+//         inputOptions:{
+//            123456: "Abono hecho",
+//            1234567: "Abono hecho",
+//            12345678:"Abono aún no efectuado",
+//         },
+       
+//        //  customClass:
+//        // 	container:
+//        // 	popup:
+//        // 	header:
+//        // 	title:
+//        // 	closeButton:
+//        // 	icon:
+//        // 	image:
+//        // 	content:
+//        // 	input:
+//        // 	actions:
+//        // 	confirmButton:
+//        // 	cancelButton:
+//        // 	footer:	
+   
+//        // showConfirmButton:
+//        // confirmButtonColor:
+//        // confirmButtonAriaLabel:
+   
+//        // showCancelButton:
+//        // cancelButtonText:
+//        // cancelButtonColor:
+//        // cancelButtonAriaLabel:
+       
+//        // buttonsStyling:
+//        // showCloseButton:
+//        // closeButtonAriaLabel:
+   
+   
+//        // imageUrl:
+//        // imageWidth:
+//        // imageHeight:
+//        // imageAlt:
+//    });
+   
+//    if (dni) {
+//       alert("hola")
+//     }else{
+//       alert("Dni no identificado")
+//     }
+// }) ()
+
+
+
